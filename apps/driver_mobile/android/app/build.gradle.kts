3672e33421e59ebae49fb1763337828cb47270e1
FairGo Dev <fairgo-dev@fairgo.in>
2026-09-19 06:50:26 +0530

    Set NDK version to 30 in Android build scripts

--- apps/driver_mobile/android/app/build.gradle.kts
+++ apps/driver_mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "in.fairgo.driver_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
--- apps/rider_mobile/android/app/build.gradle.kts
+++ apps/rider_mobile/android/app/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 android {
     namespace = "in.fairgo.rider_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "30"
 
     compileOptions {
         isCoreLibraryDesugaringEnabled = true
